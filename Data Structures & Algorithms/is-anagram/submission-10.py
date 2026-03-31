debe88f6a6efa4832550c4e853ab9f85683b3e38
Sarath465 <116046201+Sarath465@users.noreply.github.com>
2026-03-31 05:55:11 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d_s, d_t = {}, {}
+        if len(s) == len(t):
+            for i in range(len(s)):
+                # d_s[s[i]] = d_s.get(s[i],0) + 1
+                # d_t[t[i]] = d_t.get(t[i],0) + 1
+                if s[i] in d_s:
+                    d_s[s[i]] = d_s[s[i]] + 1
+                else:
+                    d_s[s[i]] = 1
+                if t[i] in d_t:
+                    d_t[t[i]] = d_t[t[i]] + 1
+                else:
+                    d_t[t[i]] = 1
+            return d_s == d_t
+        return False
